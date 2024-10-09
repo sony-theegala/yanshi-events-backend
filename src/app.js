22d0d8096bf082487e6b50fc182428bca821a7a9
Sony <sonyreddy4525@gmail.com>
2024-10-09 17:18:15 +0530

    name

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(logger);
 
 app.get("/", (req, res) => {
-    res.send("Hello from yanshi-events-backend");
+    res.send("Hello from yanshii-events-backend");
   });
 
 app.use("/categories", categoryRoutes);
